30d5e987670bc2bae0c9dd0a5b100a15a2014229
robinjosephbrown <154822011+robinjosephbrown@users.noreply.github.com>
2026-07-03 19:53:51 -0300

    main.py

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 YOUR_CHAT_ID = 6807975624
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    async def on_start(app):
-    await app.bot.send_message(
-        chat_id=YOUR_CHAT_ID,
-        text="I'm online. I'm watching."
-    )
     chat_id = update.effective_chat.id
     await update.message.reply_text(f"alive. chat_id = {chat_id}")
 
+async def post_init(app):
+    await app.bot.send_message(
+        chat_id=YOUR_CHAT_ID,
+        text="I'm alive now"
+    )
+
 def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.post_init = on_start
+    app = ApplicationBuilder().token(BOT_TOKEN).post_init(post_init).build()
 
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
