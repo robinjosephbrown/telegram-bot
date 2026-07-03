c3efcaed01aa6b947807ef6afae736f05ff87f15
robinjosephbrown <154822011+robinjosephbrown@users.noreply.github.com>
2026-07-03 18:41:06 -0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # OR paste token directly
+BOT_TOKEN = "8826394576:AAFZk1x6cKXHajrIRHdxWIqkjj8EvJFdgD8"
+PORT = int(os.environ.get("PORT", "10000"))
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("I am alive")
@@ -10,5 +11,10 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT, reply))
 
-print("Bot running...")
-app.run_polling()
+print("Bot running (webhook mode ready)")
+app.run_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    url_path=BOT_TOKEN,
+    webhook_url=f"https://telegram-bot-iibh.onrender.com/{BOT_TOKEN}"
+)
