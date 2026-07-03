bbab6abdb3041ab2a13ea6198dd011024fd43548
robinjosephbrown <154822011+robinjosephbrown@users.noreply.github.com>
2026-07-03 18:24:09 -0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("8826394576:AAFZk1x6cKXHajrIRHdxWIqkjj8EvJFdgD8")
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # OR paste token directly
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("I am alive")
@@ -10,4 +10,5 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT, reply))
 
+print("Bot running...")
 app.run_polling()
