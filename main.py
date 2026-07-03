f50e59c18cb68d19d07380261c9110656273e527
robinjosephbrown <154822011+robinjosephbrown@users.noreply.github.com>
2026-07-03 18:55:55 -0300

    main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import os
 
-app_web = Flask(__name__)
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("I am alive")
 
-bot = ApplicationBuilder().token(BOT_TOKEN).build()
-bot.add_handler(MessageHandler(filters.TEXT, reply))
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
-# Web server for Render
-@app_web.route("/")
-def home():
-    return "Bot is alive"
-
-print("Bot running")
-
-bot.run_polling()
+print("Bot running...")
+app.run_polling()
